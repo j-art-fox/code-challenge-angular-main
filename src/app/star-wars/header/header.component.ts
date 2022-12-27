e47b6ca0f4085d3a8ef6f66e9f5a48458e3e5691
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-27 15:48:07 -0700

    modified header

--- src/app/star-wars/header/header.component.ts
+++ src/app/star-wars/header/header.component.ts
@@ -7,34 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   planetsDropDown: boolean;
-  dropDownClass="hidden";
-  navData = [
-    { text: 'home', href: '/', class: 'class="mr-5 hover:text-gray-900"' },
-    {
-      text: 'planets',
-      href: '/planets',
-      class: 'class="mr-5 hover:text-gray-900"',
-    },
-    {
-      text: 'about',
-      href: '/about',
-      class: 'class="mr-5 hover:text-gray-900"',
-    },
+  dropDownClass = 'hidden';
+
+  planets: any = [
+    { name: 'Tatooine' },
+    { name: 'Alderaan' },
+    { name: 'Yavin IV' },
+    { name: 'Hoth' },
+    { name: 'Dagobah' },
+    { name: 'Bespin' },
+    { name: 'Endor' },
+    { name: 'Naboo' },
+    { name: 'Coruscant' },
   ];
-  
+
   changeLogo: boolean;
   constructor() {
     this.changeLogo = false;
     this.planetsDropDown = false;
-    
   }
-  showAndHide(){
-    if(this.planetsDropDown) {
+  showAndHide() {
+    if (this.planetsDropDown) {
       this.planetsDropDown = false;
-      this.dropDownClass = "hidden";
+      this.dropDownClass = 'hidden';
     } else {
       this.planetsDropDown = true;
-      this.dropDownClass = "absolute -translate-x-8 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 ease-in-out duration:200";
+      this.dropDownClass =
+        'drop-down-bg font-chakrapetch absolute z-10 w-44 translate-x-14 rounded divide-y divide-gray-100 shadow ease-in-out duration:200';
     }
   }
 
