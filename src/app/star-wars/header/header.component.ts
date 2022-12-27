c721a3a61b730b0a3822c5bff933381695e7fd64
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2022-12-27 12:09:40 -0700

    started drop down menus

--- src/app/star-wars/header/header.component.ts
+++ src/app/star-wars/header/header.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  planetsDropDown: boolean;
+  dropDownClass="hidden";
   navData = [
     { text: 'home', href: '/', class: 'class="mr-5 hover:text-gray-900"' },
     {
@@ -19,9 +21,21 @@ export class HeaderComponent implements OnInit {
       class: 'class="mr-5 hover:text-gray-900"',
     },
   ];
+  
   changeLogo: boolean;
   constructor() {
     this.changeLogo = false;
+    this.planetsDropDown = false;
+    
+  }
+  showAndHide(){
+    if(this.planetsDropDown) {
+      this.planetsDropDown = false;
+      this.dropDownClass = "hidden";
+    } else {
+      this.planetsDropDown = true;
+      this.dropDownClass = "absolute -translate-x-8 z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 ease-in-out duration:200";
+    }
   }
 
   ngOnInit(): void {}
