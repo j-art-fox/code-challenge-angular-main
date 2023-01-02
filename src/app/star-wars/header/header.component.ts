4e8dfd07a8c9a9ce14175deea0df08e368d38cc8
Jordan Ring-Sakabe <jordanringsakabe@gmail.com>
2023-01-02 11:47:20 -0700

    cleaned up header code

--- src/app/star-wars/header/header.component.ts
+++ src/app/star-wars/header/header.component.ts
@@ -1,28 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { PlanetsService } from '../planets.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
-  @Input() planets: any = [];
-  planetsDropDown: boolean;
-  dropDownClass = 'hidden';
-
-  constructor(private planetsService: PlanetsService) {
-    this.planetsDropDown = false;
-  }
-  showAndHide() {
-    if (this.planetsDropDown) {
-      this.planetsDropDown = false;
-      this.dropDownClass = 'hidden';
-    } else {
-      this.planetsDropDown = true;
-      this.dropDownClass =
-        'drop-down-bg font-chakrapetch absolute z-10 w-44 -translate-x-2 outline-rose-200 rounded-xl divide-y divide-rose-900 shadow ease-in-out duration:200';
-    }
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
